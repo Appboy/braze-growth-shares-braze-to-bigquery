6b6220fdbfc4cac1a8f8095c25ea68ec58da75f8
“zzhaobraze” <Zeyuan.zhao@braze.com>
2020-02-21 13:28:07 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,107 @@
+# Braze User Export to Google BigQuery
+This is an Google Cloud AppEngine python 3 script which automates the process of making getting Braze data to Google BigQuery.
+
+_Only standard attributes and data types(String, Number) has been tested_
+
+The following permission and Google Services are required:
+* [Google BigQuery](https://console.cloud.google.com/bigquery)
+* [Google API & Services](https://console.cloud.google.com/apis/dashboard) enabled
+* [Google Cloud Storage](https://console.cloud.google.com/storage/)
+* [Google App Engine](https://console.cloud.google.com/appengine)
+* Braze API Key with [users.export.segment](https://www.braze.com/docs/api/endpoints/export/user_data/post_users_segment/) permissions
+* Access to [Amazon AWS S3](https://console.aws.amazon.com/console/) (Required only if S3 Exports are setup)
+
+**Note: Ensure BigQuery and Cloud Storage are running from the same geo-location to avoid issues**
+
+## Process Steps
+The following is an outline of the process:
+* An API call to the [Braze User by Segment](https://www.braze.com/docs/api/endpoints/export/user_data/post_users_segment/) endpoint using a predefined [segment](https://www.braze.com/docs/user_guide/engagement_tools/segments/creating_a_segment/) with a callback to the app.
+* Waits for Braze to trigger the callback.
+	* If S3 for exports is enabled, reads from S3 (**TO BE IMPLEMENTED**).
+	* Otherwise, pulls info from a zip S3 url.
+* Converts files to a csv, and uploads to Google Cloud Storage.
+* Create temporary BigQuery table off the csv file.
+* Merge the temporary table with master table.
+
+![BrazeBigQueryProcess](/img/BrazeBigQuery.png)
+
+## Deploy to Google Cloud App Engine
+To deploy to your [Google Cloud Project](https://cloud.google.com/sdk/gcloud/reference/app/deploy), clone this report or use the link below.
+
+[![Run on Google Cloud](https://deploy.cloud.run/button.svg)](https://deploy.cloud.run)
+
+Create an `app.yaml`, see [app_example.yaml](/app_example.yaml) and deploy to your project using
+```
+gcloud app deploy
+```
+
+### app.yaml
+To set the environment variables for the script to run with, make an `app.yaml` (see [app_example.yaml](/app_example.yaml)) file in the root directly and updated the following:
+
+```
+env_variables:
+    gcsproject: [Google Project Name]
+    bigquery_dataset: [BigQuery DataSet]
+    bigquery_table: [BigQuery Destination Table]
+    bigquery_temptable_duration: [BigQuery TempTable Expiration (Seconds)]
+    brazerestendpoint: [Braze API REST Endpoint ie https://rest.braze.com/]
+    brazeapikey: [Braze API Key with User Segment Export Permissions]
+    brazesegmentid: [Braze Segment ID]
+    brazesegmentendpoint: [Braze API Endpoint ie /users/export/segment]
+    brazesegmentfields: [Braze export fields ie external_id,random_bucket,first_name]
+    brazesegmenttype: [Braze export field type ie STRING,INTEGER,STRING]
+    gcsprimarykey: [BigQuery primary key external_id]
+    gcsplatformprefix: [Google Appengine URL (Optional) ie .appspot.com]
+    gcspath: [Google Cloud Store path ie brazeexport]
+		s3enabled: [Boolean if AWS S3 is used]
+		s3accessid: [AWS Access ID]
+		s3secretkey: [AWS Secret Key]
+		s3bucketname: [AWS Bucket Name]
+		s3path: [AWS Bucket Prefix, optional]
+```
+
+#### app.yaml example
+Example:
+```
+env_variables:
+    gcsproject: BrazeBigQuery
+    bigquery_dataset: bgdataset
+    bigquery_table: mastertable
+    bigquery_temptable_duration: 86400
+    brazerestendpoint: https://rest.iad-01.braze.com/
+    brazeapikey: api-key-with-user-export-segment-permission
+    brazesegmentid: segmentidfromsegmentcreation
+    brazesegmentendpoint: /users/export/segment
+    brazesegmentfields: external_id,random_bucket,first_name
+    brazesegmenttype: STRING,INTEGER,STRING
+    gcsprimarykey: external_id
+    gcsplatformprefix: .appspot.com
+    gcspath: brazeexport
+		s3enabled: true
+    s3accessid: aws-access-id
+    s3secretkey: aws-secret-key
+    s3bucketname: bucket-name
+    s3path: brazeexports
+```
+
+### cron.yaml
+A [cron job](https://cloud.google.com/appengine/docs/flexible/python/scheduling-jobs-with-cron-yaml) can be tasked to trigger the Braze API Export using the internal `/schedule` endpoint. See [cron.yml](/cron.yaml) for example.
+```
+cron:
+- description: "schedule hourly processing of Braze exports"
+  url: /schedule
+  schedule: every 1 hours
+  retry_parameters:
+    job_retry_limit: 2
+    min_backoff_seconds: 2.5
+    max_backoff_seconds: 10
+    max_doublings: 3
+```
+
+To deploy run:
+```
+gcloud app deploy cron.yaml
+```
+
+Any updates to the `cron.yaml` will require the `cron.yaml` file to be redeployed.
+
---
+++ main.py
@@ -0,0 +1,310 @@
+from flask import Flask, render_template, request, Response, abort, jsonify
+from google.cloud import bigquery, storage
+
+import collections
+import os
+
+from datetime import datetime, timedelta
+import re
+import gzip
+import json
+import requests
+import csv
+import urllib3
+from requests.adapters import HTTPAdapter
+from requests.packages.urllib3.util.retry import Retry
+import io
+import zipfile
+import logging
+import boto3
+
+
+# If `entrypoint` is not defined in app.yaml, App Engine will look for an app
+# called `app` in `main.py`.
+app = Flask(__name__)
+
+# Endpoint to queue Braze Segment Export to be run via gcs cron job.
+@app.route('/schedule', methods=['POST','GET'])
+def queueBrazeSegmentExport():
+    # Insure origin source is from google cron job
+    if (request.headers.get('X-Appengine-Cron') is None ) and request.headers.get('X-AppEngine-QueueName') is None:
+        logging.critical("Schedule from outside Google App Engine")
+        abort(401)
+    brazeurl = os.environ['brazerestendpoint'].rstrip('/')  + os.environ['brazesegmentendpoint']
+    brazeapikey = os.environ['brazeapikey'].strip()
+    brazesegmentid = os.environ['brazesegmentid'].strip()
+    brazefields = [sf.strip() for sf in os.environ['brazesegmentfields'].split(',')]
+    if os.environ['gcsplatformprefix'] is None:
+        gcsplatformprefix = '.appspot.com'
+    else:
+        gcsplatformprefix = os.environ['gcsplatformprefix'].strip().lower()
+
+    # Generate call back url
+    callback_url = "https://{}{}/{}".format(os.environ['GOOGLE_CLOUD_PROJECT'], gcsplatformprefix, 'exportcallback' )
+    brazepayload = {
+        "api_key": brazeapikey,
+        "segment_id": brazesegmentid,
+        "fields_to_export": brazefields,
+        "callback_endpoint": callback_url,
+        "output_format": "zip"
+    }
+    brazeresponse = requests.post(brazeurl, json = brazepayload)
+    if (200 <= brazeresponse.status_code  < 300):
+        result = brazeresponse.json()
+        if 'message' in result:
+            if result['message'] == 'success':
+                resp = Response('{"message": "ok"}', status=200, mimetype='application/json')
+            else:
+                logging.error("Segment Export Failure: {}".format(json.dumps(result)))
+                resp = Response('{"message": "error"}', status=200, mimetype='application/json')
+            return resp
+        else:
+            logging.error("Segment Export Failure: {}".format(json.dumps(result)))
+            resp = Response('{"message": "error"}', status=200, mimetype='application/json')
+            return resp
+    else:
+        logging.error("Segment Export Failure Response")
+        resp = Response('{"message": "error"}', status=200, mimetype='application/json')
+        return resp
+
+# Method to process s3 url, and save to cloud storage
+def processURLGCS(url):
+    brazeexport = requests.get(url)
+    brazefields = [sf.strip() for sf in os.environ['brazesegmentfields'].split(',')]
+
+    # Create Google Cloud Storage Bucket file to save file to save to
+    storage_client = storage.Client()
+    gcsbucketname = os.environ['gcsproject'].lower().strip()
+    gcsbucketname += '.appspot.com'
+
+    gcsbucket = storage_client.get_bucket(gcsbucketname)
+    gcsfilename = ''
+    if (os.environ['gcspath']):
+        gcsfilename += os.environ['gcspath'].strip() + '/'
+    gcsbasefilename = "braze_export_{}".format(datetime.utcnow().strftime("%s"))
+    gcsfilename += gcsbasefilename + '.csv'
+    gcsfile = gcsbucket.blob(gcsfilename)
+    content_type = 'text/csv'
+
+    gcscsv = io.StringIO()
+    csvwriter = csv.writer(gcscsv, delimiter=',')
+    gcscsv.write(','.join(brazefields) + "\n")
+    with zipfile.ZipFile(io.BytesIO(brazeexport.content)) as exports:
+        for brazefiles in exports.infolist():
+            if '.txt' in brazefiles.filename:
+                with exports.open(brazefiles, 'r') as brazefile:
+                    linecount = 0
+                    for line in brazefile:
+                        linecount += 1
+                        try:
+                            line_utf8 = line.decode("utf-8").rstrip()
+                            record_json = json.loads(line_utf8)
+                            record = []
+                            for sfv in brazefields:
+                                if sfv in record_json:
+                                    record.append(record_json[sfv])
+                                else:
+                                    record.append(None)
+                            csvwriter.writerow(record)
+                            logging.info("Record {}".format(' '.join(str(x) for x in record)))
+                        except Exception as e :
+                            logging.warning("Parse error in {} line {}: {}\n\tError: {}"
+                                .format(brazefiles.filename, linecount, line_utf8, e))
+                            continue
+    try:
+        # Write file to cloud storage
+        gcsfile.upload_from_string(
+            gcscsv.getvalue(),
+            content_type = content_type,
+            predefined_acl = 'projectPrivate')
+    except Exception as e :
+        logging.error("Error saving to Google Cloud Storage: {}\n\tError: {}"
+            .format(gcsfilename, e))
+    gcscsv.close()
+    return gcsbasefilename
+
+
+# Method to process s3 bucket, and save to cloud storage
+def processS3GCS(object_prefix):
+    # Connect to S3
+    s3client = boto3.resource('s3',
+        aws_access_key_id=os.environ['s3accessid'].strip(),
+        aws_secret_access_key=os.environ['s3secretkey'].strip()
+        )
+    s3bucket = s3client.Bucket(os.environ['s3bucketname'].strip())
+    s3prefix = ''
+    if (os.environ['s3path'].strip()):
+        s3prefix = os.environ['s3path'].rstrip('/') + '/'
+    segment_id = ''
+    dateprefix = datetime.utcnow().strftime('%Y-%m-%d') + '/'
+    segmentprefix = os.environ['brazesegmentid'].strip()
+    s3prefix += "segment-export/{}/{}{}/".format(segmentprefix, dateprefix, object_prefix)
+    s3objs = s3bucket.objects.filter(Prefix=s3prefix)
+
+    # Generate header an output string variable
+    brazefields = [sf.strip() for sf in os.environ['brazesegmentfields'].split(',')]
+    gcscsv = io.StringIO()
+    csvwriter = csv.writer(gcscsv, delimiter=',')
+    gcscsv.write(','.join(brazefields) + "\n")
+
+    for s3obj in s3objs:
+        if '.zip' in s3obj.key:
+            print(s3obj.key)
+            buffer = io.BytesIO(s3obj.get()["Body"].read())
+            with zipfile.ZipFile(buffer) as exports:
+                for brazefiles in exports.infolist():
+                    if '.txt' in brazefiles.filename:
+                        with exports.open(brazefiles, 'r') as brazefile:
+                            linecount = 0
+                            for line in brazefile:
+                                linecount += 1
+                                try:
+                                    line_utf8 = line.decode("utf-8").rstrip()
+                                    record_json = json.loads(line_utf8)
+                                    record = []
+                                    for sfv in brazefields:
+                                        if sfv in record_json:
+                                            record.append(record_json[sfv])
+                                        else:
+                                            record.append(None)
+                                    csvwriter.writerow(record)
+                                    logging.info("Record {}".format(' '.join(str(x) for x in record)))
+                                except Exception as e :
+                                    logging.warning("Parse error in {} line {}: {}\n\tError: {}"
+                                        .format(brazefiles.filename, linecount, line_utf8, e))
+                                    continue
+
+    # Create Google Cloud Storage Bucket file to save file to save to
+    storage_client = storage.Client()
+    gcsbucketname = os.environ['gcsproject'].lower().strip()
+    gcsbucketname += '.appspot.com'
+    gcsbucket = storage_client.get_bucket(gcsbucketname)
+    gcsfilename = ''
+    if (os.environ['gcspath']):
+        gcsfilename += os.environ['gcspath'].strip() + '/'
+    gcsbasefilename = "braze_export_{}".format(datetime.utcnow().strftime("%s"))
+    gcsfilename += gcsbasefilename + '.csv'
+    gcsfile = gcsbucket.blob(gcsfilename)
+    content_type = 'text/csv'
+    try:
+        # Write file to cloud storage
+        gcsfile.upload_from_string(
+            gcscsv.getvalue(),
+            content_type = content_type,
+            predefined_acl = 'projectPrivate')
+    except Exception as e :
+        logging.error("Error saving to Google Cloud Storage: {}\n\tError: {}"
+            .format(gcsfilename, e))
+    gcscsv.close()
+    return gcsbasefilename
+
+# Creates temporary table linking to cloud storage file
+def createTempTable(gscfile):
+    gcsbucketname = os.environ['gcsproject'].lower().strip()
+    gcsbucketname += '.appspot.com'
+    gcsprefix = ''
+    if (os.environ['gcspath']):
+        gcsprefix += os.environ['gcspath'].strip() + '/'
+    gcsfullurl = "gs://{}/{}{}.csv".format(gcsbucketname,gcsprefix,gscfile)
+    gcsduration = int(os.environ['bigquery_temptable_duration'].strip())
+
+    # set expiration of the table
+    gcsexpiration = datetime.utcnow() + timedelta(seconds=gcsduration)
+    bgqclient = bigquery.Client()
+    bgqdataset = os.environ['bigquery_dataset'].strip()
+    bgqdatasetid = bgqclient.dataset(bgqdataset)
+    brazefields = [bf.lower().strip() for bf in os.environ['brazesegmentfields'].split(',')]
+    brazetype = [bt.upper().strip() for bt in os.environ['brazesegmenttype'].split(',')]
+
+    bgqschema = []
+    # Generate schema based on field type
+    for bf, bt in zip(brazefields, brazetype):
+        bgqschema.append(bigquery.SchemaField(bf, bt, mode="NULLABLE"))
+
+    table = bigquery.Table(bgqdatasetid.table(gscfile) , schema=bgqschema)
+    table.expires = gcsexpiration
+    external_config = bigquery.ExternalConfig("CSV")
+    external_config.options.skip_leading_rows = 1
+    external_config.source_uris = [gcsfullurl]
+    table.external_data_configuration = external_config
+    table = bgqclient.create_table(table)
+    return table
+
+# Method to merge temp table with master table
+def mergeTables(gcstable):
+    brazefielddml = os.environ['brazesegmentfields'].strip()
+    brazefields = [bf.lower().strip() for bf in brazefielddml.split(',')]
+    bgqdatasetid = os.environ['bigquery_dataset'].strip()
+    bgqdest = os.environ['bigquery_table'].strip()
+    bgqsource = gcstable.table_id
+    brazeprimaryid = os.environ['gcsprimarykey'].strip()
+    bqgmatched = []
+
+    # Generate update dml string
+    for bf in brazefields:
+        if bf != brazeprimaryid:
+            bqgmatched.append("dt.{} = st.{}".format(bf,bf))
+    bgmatchedml = ', '.join(bqgmatched)
+
+    # Generate upsert dml query
+    bgqdml = """
+        MERGE `{}`.`{}` dt
+        USING `{}`.`{}` st
+        ON dt.{} = st.{}
+        WHEN MATCHED THEN
+          UPDATE SET {}
+        WHEN NOT MATCHED THEN
+          INSERT({})
+          VALUES({})
+      """.format(
+        bgqdatasetid, bgqdest,
+        bgqdatasetid, bgqsource,
+        brazeprimaryid,brazeprimaryid,
+        bgmatchedml, brazefielddml, brazefielddml
+        )
+    bgqclient = bigquery.Client()
+    query_job = bgqclient.query(bgqdml)
+    results = query_job.result()
+
+# Endpoint to process the callback response from the Braze export, post only
+@app.route('/exportcallback', methods=['POST'])
+def processBrazeCallBack():
+    # Queue Braze Segment Export to be run.
+    if not (request.is_json):
+        logging.error("Segment Export Failure not json: {}".format(request.data))
+        abort(401)
+    else:
+        payload = request.get_json()
+        if not payload.keys() >= {"success"}:
+            logging.error("Segment Export Failure missing response: {}".format(json.dumps(payload)))
+            abort(400)
+        # s3enabled = os.environ['s3enabled']
+        if payload['success']:
+            # Only URL without S3 works
+            if 'url' in payload:
+                gscfile = processURLGCS(payload['url'])
+
+                # Create temporary table from Google Cloud Storage
+                gcstemptable = createTempTable(gscfile.strip())
+                # Merge Table
+                mergeTables(gcstemptable)
+            else:
+                logging.error("Segment Export Failure missing response: {}".format(json.dumps(payload)))
+                abort(400)
+            resp = Response('{"message": "ok"}', status=200, mimetype='application/json')
+            return resp
+        else:
+            logging.error("Segment Export Failure not success: {}".format(json.dumps(payload)))
+            abort(400)
+
+@app.route('/')
+def error():
+    # Default page should never need to be used, so return bad request
+    abort(401)
+
+if __name__ == '__main__':
+    # This is used when running locally only. When deploying to Google App
+    # Engine, a webserver process such as Gunicorn will serve the app. This
+    # can be configured by adding an `entrypoint` to app.yaml.
+    app.run(host='127.0.0.1', port=8080, debug=True)
+
